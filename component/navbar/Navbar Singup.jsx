b5a24dc2b6e673f083c5c0960328d933c0e5e82f
Ibrahima <ibsonseck97@gmail.com>
2022-05-13 10:49:58 +0000

    update

--- component/navbar/Navbar Singup.jsx
+++ component/navbar/Navbar Singup.jsx
@@ -2,6 +2,7 @@ import { React } from "react";
 import Image from "next/image";
 import logo from "../../public/Assets/home/Logo H White Transparent.png";
 import style from "./navbar.module.css";
+import Link from "next/link";
 
 export default function Navbar({ children }) {
 	return (
@@ -9,16 +10,16 @@ export default function Navbar({ children }) {
 			<div>
 				<div className={`${style.navbar} ${style.NAVBARTYPE}`}>
 					<div className={`${style.navbar_logo_type_content}`}>
-						<a
-							href='/'
-							className={`${style.navbar_logo_type_content_inner_logo}`}>
-							<Image src={logo} alt='' />
-						</a>
+						<Link href='/'>
+							<a className={`${style.navbar_logo_type_content_inner_logo}`}>
+								<Image src={logo} alt='' />
+							</a>
+						</Link>
 					</div>
 					<div className={`${style.burger_Navbar}`}>
-						<a href={`${process.env.D_URL}`} className={`${style.get_started_btn3}`}>
-							Sign In
-						</a>
+						<Link href={process.env.D_URL}>
+							<a className={`${style.get_started_btn3}`}>Sign In</a>
+						</Link>
 					</div>
 				</div>
 			</div>
