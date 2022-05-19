cce2251b50e529d9fcf84b941cfb36699d9f6ba7
unknown <yorickyves42@gmail.com>
2022-05-19 05:03:59 +0000

    new menu

--- component/navbar/Navbar.jsx
+++ component/navbar/Navbar.jsx
@@ -56,108 +56,110 @@ export default function Navbar({ children }) {
                     </a>
                   </div>
                   <div
-				  onClick={()=>SetToglleNav(false)}
+                    onClick={() => SetToglleNav(false)}
                     className={`${style.navbar_logo_type_content_in_closer_logo_close}`}
                   >
                     <Image src={closer}></Image>
                   </div>
                 </div>
-                <ul className={`${style.nav_bar_link_type}`}>
-                  <li className="nav_item">
-                    <Link href="/retailers">
-                      <a
-                        onClick={() => ChangeActiveLink("retailers")}
-                        className={`${
-                          active === "retailers"
-                            ? `${style.nav_link} ${style.active}`
-                            : `${style.nav_link}`
-                        }`}
-                      >
-                        Retailers
-                      </a>
-                    </Link>
-                  </li>
-                  <li className="nav_item">
-                    <Link href="/partners">
-                      <a
-                        onClick={() => ChangeActiveLink("partners")}
-                        className={`${
-                          active === "partners"
-                            ? `${style.nav_link} ${style.active}`
-                            : `${style.nav_link}`
-                        }`}
-                      >
-                        Partners
-                      </a>
-                    </Link>
-                  </li>
-                  <li className="nav_item">
-                    <a
-                      href={`${process.env.D_URL}/tracking`}
-                      onClick={() => ChangeActiveLink("tracking")}
-                      className={`${
-                        active === "tracking"
-                          ? `${style.nav_link} ${style.active}`
-                          : `${style.nav_link}`
-                      }`}
-                    >
-                      Tracking
-                    </a>
-                  </li>
-                  <li className={`${style.drop_down_company}`}>
-                    <a
-                      className={`${
-                        active === "about" || active === "contact"
-                          ? `${style.nav_link} ${style.active}`
-                          : `${style.nav_link}`
-                      }`}
-                    >
-                      Company
-                      <span className={`${style.Drop_icon}`}>
-                        <svg
-                          width="10"
-                          height="9"
-                          viewBox="0 0 16 9"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
+                <div className={`${style.links_and_btngetStarted_in_menu}`}>
+                  <ul className={`${style.nav_bar_link_type}`}>
+                    <li className="nav_item">
+                      <Link href="/retailers">
+                        <a
+                          onClick={() => ChangeActiveLink("retailers")}
+                          className={`${
+                            active === "retailers"
+                              ? `${style.nav_link} ${style.active}`
+                              : `${style.nav_link}`
+                          }`}
                         >
-                          <path
-                            d="M15.7071 1.70711C16.0976 1.31658 16.0976 0.683417 15.7071 0.292893C15.3166 -0.0976311 14.6834 -0.0976311 14.2929 0.292893L15.7071 1.70711ZM8 8L7.29289 8.70711C7.48043 8.89464 7.73478 9 8 9C8.26522 9 8.51957 8.89464 8.70711 8.70711L8 8ZM1.70711 0.292893C1.31658 -0.0976311 0.683417 -0.0976311 0.292893 0.292893C-0.0976311 0.683417 -0.0976311 1.31658 0.292893 1.70711L1.70711 0.292893ZM14.2929 0.292893L7.29289 7.29289L8.70711 8.70711L15.7071 1.70711L14.2929 0.292893ZM8.70711 7.29289L1.70711 0.292893L0.292893 1.70711L7.29289 8.70711L8.70711 7.29289Z"
-                            fill="white"
-                          />
-                        </svg>
-                      </span>
-                    </a>
-                    <div className={`${style.drop_down_company_dropdown}`}>
-                      <div>
-                        <Link href="/about_us">
-                          <a
-                            onClick={() => ChangeActiveLink("about")}
-                            className={`${style.nav_link} ${style.navlink2}`}
+                          Retailers
+                        </a>
+                      </Link>
+                    </li>
+                    <li className="nav_item">
+                      <Link href="/partners">
+                        <a
+                          onClick={() => ChangeActiveLink("partners")}
+                          className={`${
+                            active === "partners"
+                              ? `${style.nav_link} ${style.active}`
+                              : `${style.nav_link}`
+                          }`}
+                        >
+                          Partners
+                        </a>
+                      </Link>
+                    </li>
+                    <li className="nav_item">
+                      <a
+                        href={`${process.env.D_URL}/tracking`}
+                        onClick={() => ChangeActiveLink("tracking")}
+                        className={`${
+                          active === "tracking"
+                            ? `${style.nav_link} ${style.active}`
+                            : `${style.nav_link}`
+                        }`}
+                      >
+                        Tracking
+                      </a>
+                    </li>
+                    <li className={`${style.drop_down_company}`}>
+                      <a
+                        className={`${
+                          active === "about" || active === "contact"
+                            ? `${style.nav_link} ${style.active}`
+                            : `${style.nav_link}`
+                        }`}
+                      >
+                        Company
+                        <span className={`${style.Drop_icon}`}>
+                          <svg
+                            width="10"
+                            height="9"
+                            viewBox="0 0 16 9"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
                           >
-                            About us
-                          </a>
-                        </Link>
-                        <Link href="/contact_us">
-                          <a
-                            onClick={() => ChangeActiveLink("contact")}
-                            className={`${style.nav_link} ${style.navlink2}`}
-                          >
-                            Contact us
-                          </a>
-                        </Link>
+                            <path
+                              d="M15.7071 1.70711C16.0976 1.31658 16.0976 0.683417 15.7071 0.292893C15.3166 -0.0976311 14.6834 -0.0976311 14.2929 0.292893L15.7071 1.70711ZM8 8L7.29289 8.70711C7.48043 8.89464 7.73478 9 8 9C8.26522 9 8.51957 8.89464 8.70711 8.70711L8 8ZM1.70711 0.292893C1.31658 -0.0976311 0.683417 -0.0976311 0.292893 0.292893C-0.0976311 0.683417 -0.0976311 1.31658 0.292893 1.70711L1.70711 0.292893ZM14.2929 0.292893L7.29289 7.29289L8.70711 8.70711L15.7071 1.70711L14.2929 0.292893ZM8.70711 7.29289L1.70711 0.292893L0.292893 1.70711L7.29289 8.70711L8.70711 7.29289Z"
+                              fill="white"
+                            />
+                          </svg>
+                        </span>
+                      </a>
+                      <div className={`${style.drop_down_company_dropdown}`}>
+                        <div>
+                          <Link href="/about_us">
+                            <a
+                              onClick={() => ChangeActiveLink("about")}
+                              className={`${style.nav_link} ${style.navlink2}`}
+                            >
+                              About us
+                            </a>
+                          </Link>
+                          <Link href="/contact_us">
+                            <a
+                              onClick={() => ChangeActiveLink("contact")}
+                              className={`${style.nav_link} ${style.navlink2}`}
+                            >
+                              Contact us
+                            </a>
+                          </Link>
+                        </div>
                       </div>
-                    </div>
-                  </li>
-                  <li className={`${style.drop_down_company}`}>
+                    </li>
+                  </ul>
+                  <div className={`${style.get_started_btn_container_in_menu}`}>
                     <a
                       href={`${process.env.D_URL}`}
                       className={`${style.get_started_btn2}`}
                     >
                       Get Started
                     </a>
-                  </li>
-                </ul>
+                  </div>
+                </div>
               </div>
             </div>
             <div
